99171035e96b27646e94c90b50662ecf3b65e0ed
budiony <budiony@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-13 10:57:19 +0000

    Fix typo
    git-svn-id: https://plugins.svn.wordpress.org/bodi0s-easy-cache/trunk@874657 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/func.php
+++ lib/func.php
@@ -141,7 +141,7 @@ if (!function_exists("easy_cache_optimize_css_files")) {
 	$buffer = str_replace(array(', ',' ,',' , '), ',', $buffer);
 	$buffer = str_replace(array('> ',' >',' > '), '>', $buffer);
 	//Optimize CSS units
-	$buffer = str_replace(array(' 0%',' 0in',' 0cm',' 0mm',' 0em',' 0ex',' 0pt',' 0pc',' 0px'), '0', $buffer);
+	//$buffer = str_replace(array(' 0%',' 0in',' 0cm',' 0mm',' 0em',' 0ex',' 0pt',' 0pc',' 0px'), '0', $buffer);
 	$buffer = str_replace(array(',0%',',0in',',0cm',',0mm',',0em',',0ex',',0pt',',0pc',',0px'), ',0', $buffer);
 	$buffer = str_replace(array(':0%',':0in',':0cm',':0mm',':0em',':0ex',':0pt',':0pc',':0px'), ':0', $buffer);
 	//Optimize CSS colors
